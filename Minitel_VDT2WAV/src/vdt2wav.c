7d2030af9d628258edf70bca9040875cbd5c53a9
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2022-01-28 00:07:26 +0100

    vdt to wave converter : generate wave file to replay vdt file on the Minitel.

---
+++ Minitel_VDT2WAV/src/vdt2wav.c
@@ -0,0 +1,621 @@
+/*
+//
+// Copyright (C) 2022 Jean-François DEL NERO
+//
+// This file is part of vdt2wav.
+//
+// vdt2wav may be used and distributed without restriction provided
+// that this copyright statement is not removed from the file and that any
+// derivative work contains the original copyright notice and the associated
+// disclaimer.
+//
+// vdt2wav is free software; you can redistribute it
+// and/or modify  it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// vdt2wav is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with vdt2wav; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+*/
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+
+#include <stdint.h>
+#include <math.h>
+
+#include "wave.h"
+
+#define DEFAULT_NUMBER_OF_CHANNELS 1
+#define DEFAULT_SAMPLERATE 11025
+
+#define PI 3.1415926535897932384626433832795
+
+int verbose;
+
+#define FILE_CACHE_SIZE (512)
+
+typedef struct file_cache_
+{
+	FILE * f;
+	unsigned int  current_offset;
+	unsigned int  cur_page_size;
+	unsigned int  file_size;
+	unsigned char cache_buffer[FILE_CACHE_SIZE];
+	int dirty;
+}file_cache;
+
+typedef struct wavegen_
+{
+	double sinoffset;
+	double Frequency,OldFrequency;
+
+	int sample_rate;
+	int Amplitude;
+
+	int baud_rate;
+
+	int bit_time;
+	int cur_bit_time;
+	int cur_bit;
+
+	double one_freq;
+	double zero_freq;
+	double idle_freq;
+
+	int tx_buffer[64];
+	int tx_buffer_size;
+	int tx_buffer_offset;
+
+	int data_buffer_offset;
+
+	int start_delay;
+	int start_delay_cnt;
+
+	int stop_delay;
+	int stop_delay_cnt;
+
+	int all_data_sent;
+
+}wavegen;
+
+int open_file(file_cache * fc, char* path,unsigned char fill)
+{
+	memset(fc,0,sizeof(file_cache));
+
+	// Read mode
+	fc->f = fopen(path,"rb");
+	if(fc->f)
+	{
+		fseek(fc->f,0,SEEK_END);
+		fc->file_size = ftell(fc->f);
+		fseek(fc->f,fc->current_offset,SEEK_SET);
+
+		if(fc->current_offset + FILE_CACHE_SIZE > fc->file_size)
+			fc->cur_page_size = ( fc->file_size - fc->current_offset);
+		else
+			fc->cur_page_size = FILE_CACHE_SIZE;
+
+		memset(&fc->cache_buffer,fill,FILE_CACHE_SIZE);
+
+		if(!fc->file_size)
+			goto error;
+
+		if( fread(&fc->cache_buffer,fc->cur_page_size,1,fc->f) != 1 )
+			goto error;
+
+		return 0;
+	}
+
+
+
+	return -1;
+
+error:
+	if(fc->f)
+		fclose(fc->f);
+
+	fc->f = 0;
+
+	return -1;
+}
+
+unsigned char get_byte(file_cache * fc,unsigned int offset, int * success)
+{
+	unsigned char byte;
+	int ret;
+
+	byte = 0xFF;
+	ret = 1;
+
+	if(fc)
+	{
+		if(offset < fc->file_size)
+		{
+			if( ( offset >= fc->current_offset ) &&
+				( offset <  (fc->current_offset + FILE_CACHE_SIZE) ) )
+			{
+				byte = fc->cache_buffer[ offset - fc->current_offset ];
+			}
+			else
+			{
+				fc->current_offset = (offset & ~(FILE_CACHE_SIZE-1));
+				fseek(fc->f, fc->current_offset,SEEK_SET);
+
+				memset(&fc->cache_buffer,0xFF,FILE_CACHE_SIZE);
+
+				if(fc->current_offset + FILE_CACHE_SIZE < fc->file_size)
+					ret = fread(&fc->cache_buffer,FILE_CACHE_SIZE,1,fc->f);
+				else
+					ret = fread(&fc->cache_buffer,fc->file_size - fc->current_offset,1,fc->f);
+
+				byte = fc->cache_buffer[ offset - fc->current_offset ];
+			}
+		}
+	}
+
+	if(success)
+	{
+		*success = ret;
+	}
+
+	return byte;
+}
+
+void close_file(file_cache * fc)
+{
+	if(fc)
+	{
+		if(fc->f)
+		{
+			fclose(fc->f);
+
+			fc->f = NULL;
+		}
+	}
+
+	return;
+}
+
+int isOption(int argc, char* argv[],char * paramtosearch,char * argtoparam)
+{
+	int param=1;
+	int i,j;
+
+	char option[512];
+
+	memset(option,0,512);
+	while(param<=argc)
+	{
+		if(argv[param])
+		{
+			if(argv[param][0]=='-')
+			{
+				memset(option,0,512);
+
+				j=0;
+				i=1;
+				while( argv[param][i] && argv[param][i]!=':')
+				{
+					option[j]=argv[param][i];
+					i++;
+					j++;
+				}
+
+				if( !strcmp(option,paramtosearch) )
+				{
+					if(argtoparam)
+					{
+						if(argv[param][i]==':')
+						{
+							i++;
+							j=0;
+							while( argv[param][i] )
+							{
+								argtoparam[j]=argv[param][i];
+								i++;
+								j++;
+							}
+							argtoparam[j]=0;
+							return 1;
+						}
+						else
+						{
+							return -1;
+						}
+					}
+					else
+					{
+						return 1;
+					}
+				}
+			}
+		}
+		param++;
+	}
+
+	return 0;
+}
+
+void printhelp(char* argv[])
+{
+	printf("Options:\n");
+	printf("  -vdt:file \t\t\t: vdt file\n");
+	printf("  -wave:file \t\t\t: wave file\n");
+	printf("  -help \t\t\t: This help\n");
+	printf("\n");
+}
+
+int write_wave_file(char* filename,short * wavebuf,int size)
+{
+	FILE * f;
+	wav_hdr wavhdr;
+
+	memset(&wavhdr,0,sizeof(wavhdr));
+
+	f = fopen(filename,"r+");
+	if(f)
+	{
+		if(fread(&wavhdr,sizeof(wav_hdr),1,f) != 1)
+		{
+			fclose(f);
+			return -1;
+		}
+
+		wavhdr.ChunkSize += size*2;
+		wavhdr.Subchunk2Size += size*2;
+
+		fseek(f,0,SEEK_END);
+
+		fwrite(wavebuf,size*2,1,f);
+
+		fseek(f,0,SEEK_SET);
+
+		fwrite((void*)&wavhdr,sizeof(wav_hdr),1,f);
+
+		fclose(f);
+	}
+	else
+	{
+		f = fopen(filename,"wb");
+		if(f)
+		{
+			memcpy((char*)&wavhdr.RIFF,"RIFF",4);
+			memcpy((char*)&wavhdr.WAVE,"WAVE",4);
+			memcpy((char*)&wavhdr.fmt,"fmt ",4);
+			wavhdr.Subchunk1Size = 16;
+			wavhdr.AudioFormat = 1;
+			wavhdr.NumOfChan = DEFAULT_NUMBER_OF_CHANNELS;
+			wavhdr.SamplesPerSec = DEFAULT_SAMPLERATE;
+			wavhdr.bitsPerSample = 16;
+			wavhdr.bytesPerSec = ((DEFAULT_SAMPLERATE*wavhdr.bitsPerSample)/8);
+			wavhdr.blockAlign = (wavhdr.bitsPerSample/8);
+			memcpy((char*)&wavhdr.Subchunk2ID,"data",4);
+
+			wavhdr.ChunkSize = size*2 + sizeof(wav_hdr) - 8;
+			wavhdr.Subchunk2Size = size*2;
+
+			fwrite((void*)&wavhdr,sizeof(wav_hdr),1,f);
+
+			fwrite(wavebuf,size*2,1,f);
+
+			fclose(f);
+		}
+		else
+			return -1;
+	}
+
+	return 0;
+}
+
+int prepare_next_word(int * tx_buffer,unsigned char byte, int msb_first, int pre_idle, int nb_bits, int start_bits, int parity, int stop_bits, int post_idle)
+{
+	int i,j;
+	unsigned char mask;
+	int one_bits;
+	int tx_buffer_size;
+
+	tx_buffer_size = pre_idle + start_bits +  nb_bits + stop_bits + post_idle;
+	if(parity)
+		tx_buffer_size++;
+
+	if(tx_buffer_size > 64)
+	{
+		tx_buffer_size = 0;
+		return tx_buffer_size;
+	}
+
+	j = 0;
+	for(i=0;i<pre_idle;i++)
+	{
+		tx_buffer[j++] = 2;
+	}
+
+	for(i=0;i<start_bits;i++)
+	{
+		tx_buffer[j++] = 0;
+	}
+
+	one_bits = 0;
+	if(msb_first)
+	{
+		mask = 0x01 << (nb_bits - 1);
+
+		for(i=0;i<nb_bits;i++)
+		{
+			if(byte & mask)
+			{
+				tx_buffer[j++] = 1;
+				one_bits++;
+			}
+			else
+			{
+				tx_buffer[j++] = 0;
+			}
+
+			mask >>= 1;
+		}
+	}
+	else
+	{
+		mask = 0x01;
+
+		for(i=0;i<nb_bits;i++)
+		{
+			if(byte & mask)
+			{
+				tx_buffer[j++] = 1;
+				one_bits++;
+			}
+			else
+			{
+				tx_buffer[j++] = 0;
+			}
+
+			mask <<= 1;
+		}
+	}
+
+	if(parity)
+	{
+		switch(parity)
+		{
+			case 1: // odd
+				tx_buffer[j++] = one_bits & 1;
+			break;
+
+			case 2: // even
+				tx_buffer[j++] = (one_bits & 1)^1;
+			break;
+		}
+	}
+
+	for(i=0;i<stop_bits;i++)
+	{
+		tx_buffer[j++] = 1;
+	}
+
+	for(i=0;i<post_idle;i++)
+	{
+		tx_buffer[j++] = 2;
+	}
+
+	tx_buffer_size = j;
+
+	return tx_buffer_size;
+}
+
+int FillWaveBuffer(wavegen * wg,file_cache * fc,short * wave_buf, int Size)
+{
+	int i;
+
+	if(wg->Frequency!=0 && (wg->Frequency!=wg->OldFrequency))
+	{   // Sync the frequency change...
+		wg->sinoffset = (wg->OldFrequency*wg->sinoffset) / wg->Frequency;
+		wg->OldFrequency = wg->Frequency;
+	}
+
+	for(i=0;i<Size;i++)
+	{
+		wg->sinoffset++;
+
+		*(wave_buf+i) = (int)(sin((double)((double)2*(double)PI*((double)wg->Frequency)*(double)wg->sinoffset)/(double)wg->sample_rate)*(double)wg->Amplitude);
+
+		if( (wg->start_delay_cnt > wg->start_delay) && !wg->all_data_sent )
+		{
+			wg->cur_bit_time++;
+
+			if( wg->cur_bit_time >= wg->bit_time )
+			{
+				if(wg->tx_buffer_offset >= wg->tx_buffer_size)
+				{
+					if(wg->data_buffer_offset < fc->file_size)
+					{
+						wg->tx_buffer_size = prepare_next_word((int*)&wg->tx_buffer,get_byte(fc,wg->data_buffer_offset, NULL), 0, 1, 7, 1, 1, 1, 1);
+						wg->tx_buffer_offset = 0;
+						wg->data_buffer_offset++;
+					}
+					else
+					{
+						wg->all_data_sent = 1;
+					}
+				}
+
+				wg->cur_bit_time = 0;
+
+				if(wg->tx_buffer_size)
+				{
+					switch(wg->tx_buffer[wg->tx_buffer_offset])
+					{
+						case 0:
+							wg->Frequency = wg->zero_freq;
+						break;
+						case 1:
+							wg->Frequency = wg->one_freq;
+						break;
+						default:
+							wg->Frequency = wg->idle_freq;
+						break;
+					}
+
+					wg->tx_buffer_offset++;
+				}
+
+				if(wg->Frequency!=0 && wg->Frequency!=wg->OldFrequency)
+				{   // Sync the frequency change...
+					wg->sinoffset = (wg->OldFrequency*wg->sinoffset)/wg->Frequency;
+					wg->OldFrequency = wg->Frequency;
+				}
+			}
+
+		}
+		else
+		{
+			if(wg->start_delay_cnt <= wg->start_delay)
+			{
+				wg->Frequency = wg->idle_freq;
+				if(wg->Frequency!=0 && (wg->Frequency != wg->OldFrequency))
+				{   // Sync the frequency change...
+					wg->sinoffset = (wg->OldFrequency*wg->sinoffset) / wg->Frequency;
+					wg->OldFrequency = wg->Frequency;
+				}
+				wg->start_delay_cnt++;
+			}
+			else
+			{
+				if(wg->stop_delay_cnt <= wg->stop_delay)
+				{
+					wg->Frequency = wg->idle_freq;
+					if(wg->Frequency!=0 && (wg->Frequency != wg->OldFrequency))
+					{   // Sync the frequency change...
+						wg->sinoffset = (wg->OldFrequency*wg->sinoffset) / wg->Frequency;
+						wg->OldFrequency = wg->Frequency;
+					}
+					wg->stop_delay_cnt++;
+				}
+				else
+				{
+					return i;
+				}
+			}
+		}
+	}
+
+	return i;
+}
+
+int main(int argc, char* argv[])
+{
+	char filename[512];
+	char ofilename[512];
+	char tmp[512];
+
+	short wavbuf[8*1024];
+
+	file_cache fc;
+	wavegen wg;
+
+	unsigned int size;
+	int vol;
+
+	verbose = 0;
+
+	printf("Minitel VDT to WAV converter v0.1.0.1\n");
+	printf("Copyright (C) 2022 Jean-Francois DEL NERO\n");
+	printf("This program comes with ABSOLUTELY NO WARRANTY\n");
+	printf("This is free software, and you are welcome to redistribute it\n");
+	printf("under certain conditions;\n\n");
+
+	// Verbose option...
+	if(isOption(argc,argv,"verbose",0)>0)
+	{
+		printf("verbose mode\n");
+		verbose=1;
+	}
+
+
+	memset(&wg,0,sizeof(wg));
+
+	wg.zero_freq = 2100;
+	wg.one_freq = 1300;
+	wg.idle_freq = 1300;
+
+	wg.sample_rate = DEFAULT_SAMPLERATE;
+
+	if(isOption(argc,argv,"samplerate",(char*)&tmp)>0)
+	{
+		wg.sample_rate = atoi(tmp);
+	}
+
+	wg.baud_rate = 1200;
+	wg.bit_time = ((float)wg.sample_rate / (float)wg.baud_rate);
+
+	wg.Amplitude = 28000;
+	if(isOption(argc,argv,"volume",(char*)&tmp)>0)
+	{
+		vol = atoi(tmp);
+		if(vol > 100)
+			vol = 100;
+
+		wg.Amplitude = 32767 * (float)((float)vol/100);
+	}
+
+	wg.start_delay = wg.sample_rate * 2;
+	wg.stop_delay = wg.sample_rate * 1;
+
+
+	// help option...
+	if(isOption(argc,argv,"help",0)>0)
+	{
+		printhelp(argv);
+	}
+
+	memset(filename,0,sizeof(filename));
+
+	// Output file name option
+	strcpy(ofilename,"MINITEL.WAV");
+	isOption(argc,argv,"foutput",(char*)&ofilename);
+
+	if(isOption(argc,argv,"vdt",(char*)&filename)>0)
+	{
+	}
+
+	if(isOption(argc,argv,"wave",(char*)&ofilename)>0)
+	{
+		printf("Write wave file : %s from %s\n",ofilename,filename);
+
+		if(open_file(&fc, filename,0xFF)>=0)
+		{
+			do
+			{
+				size = FillWaveBuffer(&wg,&fc,(short*)&wavbuf, sizeof(wavbuf)/2);
+				write_wave_file(ofilename,(short*)&wavbuf,size);
+			}while(size == sizeof(wavbuf)/2);
+
+			close_file(&fc);
+		}
+	}
+
+	if( (isOption(argc,argv,"help",0)<=0) &&
+		(isOption(argc,argv,"vdt",0)<=0)  &&
+		(isOption(argc,argv,"wave",0)<=0) )
+	{
+		printhelp(argv);
+	}
+
+	return 0;
+}
---
+++ Minitel_VDT2WAV/src/wave.h
@@ -0,0 +1,47 @@
+/*
+//
+// Copyright (C) 2022 Jean-François DEL NERO
+//
+// This file is part of vdt2wav.
+//
+// vdt2wav may be used and distributed without restriction provided
+// that this copyright statement is not removed from the file and that any
+// derivative work contains the original copyright notice and the associated
+// disclaimer.
+//
+// vdt2wav is free software; you can redistribute it
+// and/or modify  it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// vdt2wav is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with vdt2wav; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+*/
+
+#pragma pack(1)
+
+typedef struct wav_hdr_ //
+{
+	char                     RIFF[4];        // RIFF Header
+	int                      ChunkSize;      // RIFF Chunk Size
+	char                     WAVE[4];        // WAVE Header
+	char                     fmt[4];         // FMT header
+	int                      Subchunk1Size;  // Size of the fmt chunk
+	short int                AudioFormat;    // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+	short int                NumOfChan;      // Number of channels 1=Mono 2=Stereo
+	int                      SamplesPerSec;  // Sampling Frequency in Hz
+	int                      bytesPerSec;    // bytes per second */
+	short int                blockAlign;     // 2=16-bit mono, 4=16-bit stereo
+	short int                bitsPerSample;  // Number of bits per sample
+	char                     Subchunk2ID[4]; // "data"  string
+	int                      Subchunk2Size;  // Sampled data length
+}wav_hdr;
+
+#pragma pack()
